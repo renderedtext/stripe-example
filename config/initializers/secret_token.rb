680c3619fe52eaf341ffee942ed53b4712e64e27
BM5k <me@bm5k.com>
2012-01-27 21:48:17 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StripeExample::Application.config.secret_token = '288237dcadad158d785d5deb59e862c5fdc6ceb652044dba32665e28a75d6f46aeed2c18d242864e5ca3c3bc6f842ad808b54b1b8c818e4a7be2993d732d4f58'
