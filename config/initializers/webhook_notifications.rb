4d86374d304255850dc2dad1cfe171cfc75f2815
BM5k <me@bm5k.com>
2012-02-16 20:23:52 -0600

    move webhooks.md into the wiki

--- config/initializers/webhook_notifications.rb
+++ config/initializers/webhook_notifications.rb
@@ -1,6 +1,7 @@
 module ActiveSupport
 
-  # see webhooks.md for more information on how to subscribe to webhook related notifications
+  # see https://github.com/BM5k/stripe-example/wiki/Stripe-Webhook-Notifications 
+  # for more information on how to subscribe to webhook related notifications
 
   # For example, you can log all of the stripe webhooks
   # Notifications.subscribe /^stripe/ do |name, start, finish, id, payload|
