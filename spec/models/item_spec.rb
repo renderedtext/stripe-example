e1e4d7027ab80d1dd1e3c01a8afddd9c017fb2a8
Ryan Rix <ry@n.rix.si>
2012-02-23 16:35:03 -0700

    T: Make Item spec use sexy validations

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,11 +2,6 @@ require 'spec_helper'
 
 describe Item do
 
-  it 'requires a price' do
-    Factory.build(:item, price: nil     ).should_not be_valid
-    Factory.build(:item, price: "bacon" ).should_not be_valid
-    Factory.build(:item, price: "100.00").should     be_valid
-    Factory.build(:item                 ).should     be_valid
-  end
+  its (:price) { should validate :numericality, {greater_than_or_equal_to: 0 }}
 
 end
